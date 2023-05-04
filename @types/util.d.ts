0554e6bfb1577159806a9086c44c743e552f7953
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2023-05-04 21:54:38 +0300

    add mesh and sound types

---
+++ @types/util.d.ts
@@ -0,0 +1,38 @@
+declare interface Mesh {
+  vertexes: Array<[x: number, y: number, z: number] | [x: number, y: number]>
+  segments: number[][]
+  colors?: number[]
+}
+
+declare interface Sound {
+  attack?: number
+  decay?: number
+  sustain?: number
+  sustainPunch?: number
+  amplification?: number
+  tremoloDepth?: number
+  tremoloFrequency?: number
+  frequency?: number
+  frequencyDeltaSweep?: number
+  frequencyJump1Onset?: number
+  frequencyJump2Onset?: number
+  frequencyJump1Amount?: number
+  frequencyJump2Amount?: number
+  frequencySweep?: number
+  vibratoFrequency?: number
+  vibratoDepth?: number
+  flangerOffset?: number
+  flangerOffsetSweep?: number
+  repeatFrequency?: number
+  waveform?:
+    | "sine"
+    | "triangle"
+    | "sawtooth"
+    | "square"
+    | "tangent"
+    | "whistle"
+    | "breaker"
+    | "whitenoise"
+    | "pinknoise"
+    | "brownnoise"
+}
--- level.ts
+++ level.ts
@@ -5,9 +5,3 @@ pewpew.set_level_size(width, height)
 const player = pewpew.new_player_ship(width / 2 as fx, height / 2 as fx, 0)
 pewpew.print("Hi!")
 pewpew.configure_player_ship_weapon(player, {cannon: pewpew.CannonType.HEMISPHERE, frequency: pewpew.CannonFrequency.FREQ_5})
-
-const meshes: Mesh[] = [
-  {
-    vertexes: [[]]
-  }
-]
