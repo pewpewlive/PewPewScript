75cb4c2c6b82697d7ed79157f94fa8b6d36c05a1
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2023-06-11 16:18:31 +0300

    make a smiple example level

--- level.ts
+++ level.ts
@@ -1,15 +1,29 @@
 import { randomPos } from "./helpers/lib"
 
-const width = 1000 as fx
-const height = 1000 as fx
+const width = 500 as fx
+const height = 500 as fx
 
 pewpew.set_level_size(width, height)
-const player = pewpew.new_player_ship(divFx(width, 2 as fx), divFx(height, 2 as fx), 0) // Goofy ahh type definition
-pewpew.print("Hi!")
+const player = pewpew.new_player_ship(divFx(width, 2 as fx), divFx(height, 2 as fx), 0)
+
 pewpew.configure_player_ship_weapon(player, {
-  cannon: pewpew.CannonType.HEMISPHERE,
-  frequency: pewpew.CannonFrequency.FREQ_5,
+  cannon: pewpew.CannonType.TRIPLE,
+  frequency: pewpew.CannonFrequency.FREQ_7_5,
 })
 
-const [vx, vy] = randomPos(width, height)
-pewpew.new_wary(vx, vy)
+let time = 0
+pewpew.add_update_callback(() => {
+  time++
+  if (pewpew.get_player_configuration(0).has_lost) pewpew.stop_game()
+  if (time % 30 === 0) {
+    const [rx, ry] = randomPos(width, height)
+    pewpew.new_rolling_cube(rx, ry)
+  }
+  if (time % 150 === 0) {
+    const [bx, by] = randomPos(width, height)
+    pewpew.new_bonus(bx, by, pewpew.BonusType.SHIELD, {
+      box_duration: 90,
+      number_of_shields: 1
+    })
+  }
+})
